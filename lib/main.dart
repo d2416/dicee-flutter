98337ec219578be947c2ca3aaf4e7213f27d945c
David <davidpineda.co@gmail.com>
2021-01-25 17:20:11 +0000

    show random numbers in dicees

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -22,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +34,8 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 2;
-                  print('diceNumber = $leftDiceNumber');
+                  leftDiceNumber = Random().nextInt(6) + 1;
                 });
-
-                print('The left button got pressed.');
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,9 +43,11 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('The right button got pressed.');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
