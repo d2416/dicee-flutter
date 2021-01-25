034586dfe916ac117453b1df2505e1d9a80ed3b0
David <davidpineda.co@gmail.com>
2021-01-25 15:52:02 +0000

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,28 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('The left button got pressed.');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('The right button got pressed.');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
 }
